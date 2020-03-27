9eb874785ae8685f85de884984142952c657b459
Miller Alfonso Rubio Pérez <millerdako@hotmail.com>
2020-03-26 22:04:04 -0500

    1.3
    Capitulo 4

--- GhoulieMan/Assets/Scripts/KnifeDestruction.cs
+++ GhoulieMan/Assets/Scripts/KnifeDestruction.cs
@@ -2,24 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KnifeDestruction : MonoBehaviour
-{
-    public float lifeSpan = 2.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Destroy (gameObject, lifeSpan);
-    }
+public class KnifeDestruction : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public float lifeSpan = 2.0f;
 
-    void OnTriggerEnter (Collider other){
-        if (other.gameObject){
-            Destroy (this.gameObject);
-        }
-    }
+	// Use this for initialization
+	void Start () {
+
+		Destroy (gameObject, lifeSpan);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject) {
+			Destroy (this.gameObject);
+		}
+	}
 }
+
+
