9b88d1364745761f68e267a6bcadd84229b78f6f
Miller Alfonso Rubio Pérez <millerdako@hotmail.com>
2020-03-28 20:08:03 -0500

    1.6
    Orco animation

---
+++ GhoulieMan/Assets/Scripts/Enemy03Health.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy03Health : MonoBehaviour
+{
+    [SerializeField] private int startingHealth = 20;
+    [SerializeField] private float timeSinceLastHit = 0.5f;
+    [SerializeField] private float dissapearSpeed = 2f;
+    [SerializeField] private int currentHealth;
+
+    private float timer = 0f;
+    private Animator anim;
+    private NavMeshAgent nav;
+    private bool isAlive;
+    private Rigidbody rigidbody;
+    private CapsuleCollider capsuleCollider;
+    private bool dissapearEnemy = false;
+    private BoxCollider weaponCollider;
+    private AudioSource audio;
+    public AudioClip hurtEnemyAudio;
+    public AudioClip dieEnemyAudio;
+
+    public bool IsAlive {
+        get {return isAlive;}
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        weaponCollider = GetComponentInChildren<BoxCollider>();
+        rigidbody = GetComponent <Rigidbody> ();
+        capsuleCollider = GetComponent <CapsuleCollider> ();
+        nav = GetComponent <NavMeshAgent> ();
+        anim = GetComponent <Animator> ();
+        isAlive = true;
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(dissapearEnemy){
+            transform.Translate (-Vector3.up * dissapearSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter (Collider other){
+        if(timer >= timeSinceLastHit && !GameManager.instance.GameOver){
+            if(other.tag == "PlayerWeapon"){
+                takeHit();
+                timer = 0f;
+            }
+        }
+    }
+
+    void takeHit (){
+        if(currentHealth > 0){
+            anim.Play ("Hurt");
+            currentHealth -= 10;
+            audio.PlayOneShot (hurtEnemyAudio);
+        }
+
+        if (currentHealth <= 0){
+            isAlive = false;
+            KillEnemy();
+        }
+    }
+
+    void KillEnemy(){
+        capsuleCollider.enabled = false;
+        nav.enabled = false;
+        anim.SetTrigger ("EnemyDie");
+        rigidbody.isKinematic = true;
+        weaponCollider.enabled = false;
+        StartCoroutine(removeEnemy());
+        audio.PlayOneShot (dieEnemyAudio);
+    }
+
+    IEnumerator removeEnemy(){
+        yield return new WaitForSeconds (2f);
+        dissapearEnemy = true;
+        yield return new WaitForSeconds (1f);
+        Destroy (gameObject);
+    }
+}
---
+++ GhoulieMan/Assets/Scripts/Enemy03Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy03Move : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private NavMeshAgent nav;
+    private Animator anim;
+    private Enemy03Health enemy03Health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+        enemy03Health = GetComponent<Enemy03Health> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Vector3.Distance (player.position, this.transform.position) < 12) {
+        if (!GameManager.instance.GameOver && enemy03Health.IsAlive) {
+          nav.SetDestination (player.position);
+          anim.SetBool ("isWalking", true);
+          anim.SetBool ("isIdle", false);
+        }else if (GameManager.instance.GameOver || !enemy03Health.IsAlive) {
+        anim.SetBool ("isWalking", false);
+        anim.SetBool ("isIdle", true);
+        nav.enabled = false;
+        }
+      }
+    }
+}
