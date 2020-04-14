9b3247edee7225a3fb228c46c519c29965e72b42
Miller Alfonso Rubio Pérez <millerdako@hotmail.com>
2020-04-14 09:15:19 -0500

    1.13
    LifeItem

---
+++ GhoulieMan/Assets/Scripts/Items Scripts/LifeItemScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeItemScript : MonoBehaviour
+{
+    private GameObject player;
+    private AudioSource audio;
+    private LifeManager lifeManager;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider boxCollider;
+    private AudioClip pickItem;
+    private ItemExplode itemExplode;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameManager.instance.Player;
+        lifeManager = FindObjectOfType<LifeManager>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider>();
+        audio = player.GetComponent<AudioSource>();
+        pickItem = player.GetComponent<PlayerHealth>().pickItem;
+        itemExplode = GetComponent<ItemExplode>();
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            PickLife();
+            print("Life Collected");
+        }
+    }
+    
+    public void PickLife()
+    {
+        lifeManager.GiveLife();
+        spriteRenderer.enabled = false;
+        boxCollider.enabled = false;
+        audio.PlayOneShot(pickItem);
+        itemExplode.PickUp();
+        Destroy(gameObject);
+    }
+    
+}
